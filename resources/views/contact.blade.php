6d259a3bcfdc2951e1cec115e31e769078bc24b5
Bio <biohazardhome@gmail.com>
2016-04-25 23:19:25 +0300

    Change variable  on

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,3 +1,12 @@
+@if (!empty($errors) && count($errors->all()))
+	<div class="alert alert-danger">
+		<ul>
+		    @foreach ($errors->all() as $error)
+		        <li>{{ $error }}</li>
+		    @endforeach
+		</ul>
+	</div>
+@endif
 
 <form action="/contact/store" method="POST">
 	{{ csrf_field() }}
